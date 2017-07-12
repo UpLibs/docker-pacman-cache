9c7783bb50168b7a518a90bc617a550f4e6424bb
julio <julio@uppoints.com>
2017-07-12 14:00:37 -0300

    changed cached dir location

--- run-pacman-cache.sh
+++ run-pacman-cache.sh
@@ -1,23 +1,21 @@
 #!/bin/bash -x
 
-cp -f /tmp/upp-pacman-cache/mirrorlist /etc/pacman.d/mirrorlist
+cp -f /tmp/upp-linux-build/upp-pacman-cache/mirrorlist /etc/pacman.d/mirrorlist
 
 echo "------------------------------------------------------------------"
-cat /tmp/upp-pacman-cache/pacman.conf
+cat /tmp/upp-linux-build/upp-pacman-cache/pacman.conf
 echo "------------------------------------------------------------------"
 cat /etc/pacman.d/mirrorlist
 echo "------------------------------------------------------------------"
 
-rm -f /tmp/upp-pacman-cache/cache/*
+rm -f /tmp/upp-linux-build/upp-pacman-cache/cache/*
 
-rm -f /tmp/upp-pacman-cache/sync/*.db
+rm -f /tmp/upp-linux-build/upp-pacman-cache/sync/*.db
 rm -f /var/lib/pacman/sync/*.db
 
-/usr/bin/pacman --cachedir /tmp/upp-pacman-cache/cache --config /tmp/upp-pacman-cache/pacman.conf --noconfirm -Syw \
+/usr/bin/pacman --cachedir /tmp/upp-linux-build/upp-pacman-cache/cache --config /tmp/upp-linux-build/upp-pacman-cache/pacman.conf --noconfirm -Syw \
 binutils zip unzip bzip2 xz tree nano wget traceroute v4l-utils python v4l-utils \
 git zlib autoconf automake libtool gcc make ntp \
 dhcpcd wpa_supplicant iw
 
-
-cp -f /var/lib/pacman/sync/* /tmp/upp-pacman-cache/sync
-
+cp -f /var/lib/pacman/sync/* /tmp/upp-linux-build/upp-pacman-cache/sync
