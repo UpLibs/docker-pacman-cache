801417cd8b9254ec40362c21686994322113b3bd
jorge <jorge@uppoints.com>
2019-08-30 19:31:20 -0300

    added debug flag to pacman

--- run-pacman-cache.sh
+++ run-pacman-cache.sh
@@ -12,7 +12,7 @@ rm -f /tmp/upp-linux-build/upp-pacman-cache/cache/*
 rm -f /tmp/upp-linux-build/upp-pacman-cache/sync/*.db
 rm -f /var/lib/pacman/sync/*.db
 
-/usr/bin/pacman -v --cachedir /tmp/upp-linux-build/upp-pacman-cache/cache --config /tmp/pacman.conf --noconfirm -Syw \
+/usr/bin/pacman -v --debug --cachedir /tmp/upp-linux-build/upp-pacman-cache/cache --config /tmp/pacman.conf --noconfirm -Syw \
 binutils zip unzip bzip2 xz tree nano wget traceroute v4l-utils python v4l-utils \
 git zlib autoconf automake libtool gcc make ntp \
 dhcpcd wpa_supplicant iw
